1b75025d9bb45158284f4f0810408f6920a5db93
JP Arimaa <juha-pekka.arimaa@varjo.com>
2020-08-21 20:43:58 +0300

    Use csv module to write and read csv file

--- read-csv/read-csv/read_csv.py
+++ read-csv/read-csv/read_csv.py
@@ -1,9 +1,24 @@
 import random
+import csv
 
-def write_random_csv():
-    with open("myfile.csv", "w") as f:
-        for i in range(6):
-            r = random.randrange(100)
-            f.write(f'{r},')
+def write_random_csv(path):
+    with open(path, 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',')
+        for i in range(5):
+            values = []
+            for j in range(5):
+                values.append(str(random.randrange(100)))
+            csv_writer.writerow(values)
 
-write_random_csv()
+def read_csv(path):
+    with open(path, newline='') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        values = []
+        for row in csv_reader:
+            values += row
+        return values
+
+path = 'myfile.csv'
+write_random_csv(path)
+values = read_csv(path)
+print(max(values))
