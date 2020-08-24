8039447ecf817faf4a4a7e6a11d28032780f6a06
JP Arimaa <juha-pekka.arimaa@varjo.com>
2020-08-24 21:17:22 +0300

    Fix astar bugs and add better visualization

--- a-star/a-star/astar.py
+++ a-star/a-star/astar.py
@@ -11,20 +11,72 @@ class Node():
 
     def __eq__(self, other):
         return self.position == other.position
-  
+
+class Visualizer():
 # 0 = free to move
 # 1 = blocked
 # 2 = closed
 # 3 = open
 # 4 = current
+# 5 = path
+# 6 = start, end
+    def __init__(self, end):
+        self.color_map = ListedColormap([[1, 1, 1], [0, 0, 0], [0.6, 0.6, 1], [0, 1, 0], [1, 0, 0], [1, 0, 1], [0, 1, 1]])
+        self.max_columns = 10
+        self.grid_copies = []
+        self.path = []
+        self.end = end
+
+    def add_grid(self, grid, open_list, closed_list, current_node):
+        visualize_grid = grid.copy()
+        for n in open_list:
+            visualize_grid[n.position[0]][n.position[1]] = 3
+        for n in closed_list:
+            visualize_grid[n.position[0]][n.position[1]] = 2
+        visualize_grid[current_node.position[0]][current_node.position[1]] = 4
+        visualize_grid[self.end[0]][self.end[1]] = 6
+        self.grid_copies.append(visualize_grid)
+
+    def add_path(self, path):
+        g = self.grid_copies[-1].copy()        
+        for p in path:
+            g[p[0]][p[1]] = 5
+        self.grid_copies.append(g)
+
+    def visualize(self):
+        total_count = len(self.grid_copies)
+        row_count = max(2, (total_count // self.max_columns) + 1)
+        column_count = max(2, min(total_count + 1, self.max_columns))
+        fig, axs = plt.subplots(row_count, column_count)
+        fig.set_figwidth(20)
+        last_index = 0
+        for index, grid in enumerate(self.grid_copies):
+            r = index // column_count
+            c = index % column_count
+            axs[r, c].get_xaxis().set_visible(False)
+            axs[r, c].get_yaxis().set_visible(False)
+            axs[r, c].set_title("%s" % (index + 1))
+            axs[r, c].matshow(grid, cmap=self.color_map)
+            last_index = index
+        for index in range(last_index + 1, column_count * row_count):
+            r = index // column_count
+            c = index % column_count
+            axs[r, c].axis("off")
+        plt.show()
+
+    def print(self):
+        for g in self.grid_copies:
+            print(g)
+
 def find_path(grid, width, height, start, end, visualize=True):
     start_node = Node(None, start)
     end_node = Node(None, end)
     open_list = []
     closed_list = []
     open_list.append(start_node)
-    total_loop_counter = 0
-    grid_copies = []
+    grid[start[0]][start[1]] = 6
+    grid[end[0]][end[1]] = 6
+    visualizer = Visualizer(end)
 
     while len(open_list) > 0:
         current_node = open_list[0]
@@ -43,11 +95,14 @@ def find_path(grid, width, height, start, end, visualize=True):
             while current is not None:
                 path.append(current.position)
                 current = current.parent
+            if visualize:
+                visualizer.add_path(path)
+                visualizer.visualize()
             return path[::-1]
                 
         children = []
         for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
-            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+            node_position = [current_node.position[0] + new_position[0], current_node.position[1] + new_position[1]]
 
             # Make sure within range
             if node_position[0] < 0 or node_position[1] < 0 or node_position[0] > width - 1 or node_position[1] > height - 1:
@@ -60,51 +115,34 @@ def find_path(grid, width, height, start, end, visualize=True):
             new_node = Node(current_node, node_position)
             children.append(new_node)
 
-        for child in children:            
-            for closed_child in closed_list:
-                if child == closed_child:
-                    continue
+        for child in children:
+            in_closed_list = False
+            for c in closed_list:
+                if child == c:
+                    in_closed_list = True
+                    break
+
+            if in_closed_list:
+                continue
 
             child.g = current_node.g + 1
             child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
             child.f = child.g + child.h
 
+            in_open_list = False
             for open_node in open_list:
-                if child == open_node and child.g > open_node.g:
-                    continue
+                if child == open_node and child.g >= open_node.g:
+                    in_open_list = True
+                    break
+            
+            if in_open_list:
+                continue
 
             open_list.append(child)
         
         if visualize:
-            color_map = ListedColormap([[1, 1, 1], [0, 0, 0], [0.6, 0.6, 1], [0, 1, 0], [1, 0, 0]])
-            max_columns = 10
-            row_count = max(2, (total_loop_counter // max_columns) + 1)
-            column_count = max(2, min(total_loop_counter + 1, max_columns))
-            fig, axs = plt.subplots(row_count, column_count)
-            fig.set_figwidth(20)
-            visualize_grid = grid.copy()
-            for n in open_list:
-                visualize_grid[n.position[0]][n.position[1]] = 3
-            for n in closed_list:
-                visualize_grid[n.position[0]][n.position[1]] = 2          
-            visualize_grid[current_node.position[0]][current_node.position[1]] = 4
-            grid_copies.append(visualize_grid)
-            last_index = 0
-            for index, grid in enumerate(grid_copies):
-                r = index // column_count
-                c = index % column_count
-                axs[r, c].get_xaxis().set_visible(False)
-                axs[r, c].get_yaxis().set_visible(False)
-                axs[r, c].set_title("%s" % (index + 1))
-                axs[r, c].matshow(grid, cmap=color_map)
-                last_index = index
-            for index in range(last_index + 1, column_count * row_count):
-                r = index // column_count
-                c = index % column_count
-                axs[r, c].axis("off")
-            plt.show()
-
-        total_loop_counter += 1
+            visualizer.add_grid(grid, open_list, closed_list, current_node)
+            visualizer.visualize()
 
     # Path not found
     return []
--- a-star/a-star/find_path.py
+++ a-star/a-star/find_path.py
@@ -5,20 +5,25 @@ import random
 import astar
 
 def generate_grid(row_count, column_count):
-    random.seed(3)
+    random.seed(4)
     total_count = row_count * column_count
     grid = np.zeros(total_count)
     for i in range(total_count):
         value = 0
-        if random.randrange(100) < 10:
+        if random.randrange(100) < 50:
             value = 1
         grid[i] = value
     return grid.reshape((row_count, column_count))
 
-width = 10
-height = 10
+width = 5
+height = 5
 grid = generate_grid(width, height)
+print(grid)
 #color_map = ListedColormap([[1, 1, 1], [0, 0, 0]])
 #plt.matshow(grid, cmap = color_map)
 #plt.show()
-path = astar.find_path(grid, width, height, [0,0], [width - 1, height - 1])
+path = astar.find_path(grid, width, height, [0,0], [width - 1, height - 1], True)
+if len(path) > 0:
+    print(path)
+else:
+    print("Path not found")
