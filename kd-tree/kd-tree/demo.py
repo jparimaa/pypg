70ba746289c78e31d25729891ff9ca812a53266a
JP Arimaa <juha-pekka.arimaa@varjo.com>
2020-08-29 22:08:03 +0300

    Kd: add find and find_min

--- kd-tree/kd-tree/demo.py
+++ kd-tree/kd-tree/demo.py
@@ -1,11 +1,25 @@
 import kd_tree
 
+def find(values, tree):
+    found = tree.find(values)
+    if found == None:
+        print("Not found")
+    else:
+        print("Found {} with id {}".format(values, found.id))
+
 my_2d_tree = kd_tree.KdTree(2)
 my_2d_tree.add_node([20,20])
 my_2d_tree.add_node([10,10])
 my_2d_tree.add_node([30,10])
-my_2d_tree.add_node([5,15])
+my_2d_tree.add_node([0,15])
 my_2d_tree.add_node([40,5])
 my_2d_tree.add_node([5,5])
 my_2d_tree.add_node([100,100])
+
+find([100,100], my_2d_tree)
+find([0,100], my_2d_tree)
+
+print(my_2d_tree.find_min(0).values)
+print(my_2d_tree.find_min(1).values)
+
 my_2d_tree.visualize()
--- kd-tree/kd-tree/kd_tree.py
+++ kd-tree/kd-tree/kd_tree.py
@@ -21,6 +21,45 @@ class KdTree():
             current.right = self.add_node_recursive(current.right, values, depth + 1)
         return current
 
+    def find(self, values):
+        return self.find_recursive(self.root, values, 0)
+
+    def find_recursive(self, current, values, depth):
+
+        if values == current.values:
+            return current
+        dimension = depth % self.d
+        if values[dimension] < current.values[dimension]:
+            if current.left.empty:
+                return None
+            else:
+                return self.find_recursive(current.left, values, depth + 1)
+        else:
+            if current.right.empty:
+                return None
+            else:
+                return self.find_recursive(current.right, values, depth + 1)
+
+    def find_min(self, dimension):
+        return self.find_min_recursive(self.root, dimension, 0)
+
+    def find_min_recursive(self, current, dimension, depth):
+        if current.empty:
+            return current
+        current_dimension = depth % self.d
+        if current_dimension == dimension:
+            if current.left.empty:
+                return current
+            else:
+                return get_smaller_node(current, 
+                                        self.find_min_recursive(current.left, dimension, depth + 1), 
+                                        dimension)
+
+        smallest_child = get_smaller_node(self.find_min_recursive(current.left, dimension, depth + 1), 
+                                          self.find_min_recursive(current.right, dimension, depth + 1), 
+                                          dimension)
+        return get_smaller_node(current, smallest_child, dimension)
+
     def visualize(self):
         g = graphviz.Graph('g', filename='kdtree.gv', format = "png")
         g.graph_attr['rankdir'] = 'BT'
@@ -28,7 +67,8 @@ class KdTree():
         g.view()
                 
     def visualize_recursive(self, current, parent, g):
-        list_str = ', '.join([str(elem) for elem in current.values]) 
+        list_str = current.id + "\n"
+        list_str += ', '.join([str(elem) for elem in current.values])
         g.node(current.id, list_str)
         if parent != None:
             g.edge(current.id, parent.id)
@@ -40,6 +80,7 @@ class KdTree():
 class Node():
     def __init__(self):
         self.empty = True
+        self.values = []
 
     def set_values(self, values, id):
         self.values = values
@@ -47,3 +88,13 @@ class Node():
         self.empty = False
         self.left = Node()
         self.right = Node()
+
+def get_smaller_node(n1, n2, d):
+    if n1.empty:
+        return n2
+    if n2.empty:
+        return n1
+    if n1.values[d] < n2.values[d]:
+        return n1
+    else:
+        return n2
